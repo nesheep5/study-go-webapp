1d423abed5d29d00e4c1912465efbd3dee79bf86
shogo watanabe <ne.sheep.5mouths@gmail.com>
2018-11-14 14:28:08 +0900

    add trace.

--- chat/main.go
+++ chat/main.go
@@ -5,8 +5,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/nesheep5/study-go-webapp/trace"
 )
 
 type templateHandler struct {
@@ -27,6 +30,7 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// チャットルーム開始
--- chat/room.go
+++ chat/room.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/nesheep5/study-go-webapp/trace"
 )
 
 type room struct {
@@ -16,6 +17,8 @@ type room struct {
 	leave chan *client
 	// 在室しているすべてのクライアントが保持される
 	clients map[*client]bool
+	// 操作ログ
+	tracer trace.Tracer
 }
 
 func newRoom() *room {
@@ -33,20 +36,25 @@ func (r *room) run() {
 		case client := <-r.join:
 			// 参加
 			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
 		case client := <-r.leave:
 			// 退室
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("新しいクライアントが退室しました")
 		case msg := <-r.forward:
+			r.tracer.Trace("メッセージを受信しました: ", string(msg))
 			// すべてのクライアントにメッセージを転送
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					// メッセージを送信
+					r.tracer.Trace(" -- クライアントに送信されました")
 				default:
 					// 送信に失敗
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- 送信に失敗しました。クリーンアップします。")
 				}
 			}
 		}
