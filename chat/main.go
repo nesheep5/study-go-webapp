e0317731f5eb30f24022616a7273ff2f12ee1f27
shogo watanabe <ne.sheep.5mouths@gmail.com>
2018-11-15 16:52:32 +0900

    add facebook auth.

--- chat/main.go
+++ chat/main.go
@@ -5,8 +5,12 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
 )
 
 type templateHandler struct {
@@ -26,6 +30,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
+	// gomniauthのセットアップa
+	gomniauth.SetSecurityKey("securitykey")
+	gomniauth.WithProviders(
+		facebook.New(os.Getenv("FB_KEY"), os.Getenv("FB_SECRET_KEY"), "http://localhost:8080/auth/callback/facebook"),
+		//github.New("クライアントID", "secretkey", "http://localhost:8080/auth/callback/github"),
+		//google.New("クライアントID", "secretkey", "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	// r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
