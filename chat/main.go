97211ddf5e3a954fbfd460cc86cb96b65a9946fd
shogo watanabe <ne.sheep.5mouths@gmail.com>
2018-11-15 11:28:27 +0900

    create login.html

--- chat/main.go
+++ chat/main.go
@@ -29,6 +29,7 @@ func main() {
 	r := newRoom()
 	// r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/room", r)
 	// チャットルーム開始
 	go r.run()
